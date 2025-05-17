5a41bd2cdbf1f04f26ba31418e026b537a7df425
Unuching Marma <unuching.marma2023@gmail.com>
2025-05-17 10:57:52 +0600

    update server index

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import express from "express";
 const myApp = express();
 const port = 3000;
 
+myApp.get("/", (req, res) => {
+  res.send("Hello Wold");
+});
+
 myApp.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
